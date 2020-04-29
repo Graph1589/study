cf0a96451852665ea2f6a81871b9fb6b5a928aac
Станислав Рожнов <stanislavroznov@MacBook-Air-Stanislav.local>
2020-04-29 18:49:26 +0300

    arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,25 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        return array.map { |num| num > 0 ? max : num }
       end
 
       def search(_array, _query)
-        0
+        if _array.empty? 
+          return -1
+        end
+        left = -1
+        right = _array.length
+        while left < right - 1 do
+          mid = (left + right) / 2
+          if _array[mid] < _query
+            left = mid
+          else
+            right = mid
+          end
+        end
+        return _array[mid] == _query ? right : -1
       end
     end
   end
