68e8ee3e952f4a7418850a1ae64eb6fe219ad3a1
Станислав Рожнов <stanislavroznov@MacBook-Air-Stanislav.local>
2020-04-29 19:22:37 +0300

    fixed linter

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,24 +3,23 @@ module Exercise
     class << self
       def replace(array)
         max = array.max
-        return array.map { |num| num > 0 ? max : num }
+        array.map { |num| num.positive? ? max : num }
       end
 
-      def search(_array, _query)
-        if _array.empty? 
-          return -1
-        end
+      def search(arr, query)
+        return -1 if arr.empty?
+
         left = -1
-        right = _array.length
-        while left < right - 1 do
+        right = arr.length
+        while left < right - 1
           mid = (left + right) / 2
-          if _array[mid] < _query
+          if arr[mid] < query
             left = mid
           else
             right = mid
           end
         end
-        return _array[mid] == _query ? right : -1
+        arr[mid] == query ? right : -1
       end
     end
   end
